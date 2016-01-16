3b8cf83ced4ab2fd39b3adf102f3f5c9b26798b0
Kacper Czapp <kacper1703@gmail.com>
2016-01-16 12:23:23 +0100

    Poprawa pierwszej sugestii

--- Zadanie2.playground/Contents.swift
+++ Zadanie2.playground/Contents.swift
@@ -1,19 +1,36 @@
 import UIKit
 
+
 class Fiszka {
     var tekstAwers = ""
     var tekstRewers = ""
     var czyNauczony = false
     var listaPodpowiedzi:[typPodpowiedzi] = []
     
-    enum typPodpowiedzi {
+    enum typPodpowiedzi
+    {
         case Tekst(tekst:String)
         case Obraz(obraz:UIImage)
         case Dźwięk(dźwięk:NSURL) //URL do dźwięku
         case Współrzędne(x:Float,y:Float) //przy pracy mapami (geografia, nawigacja morska)
+        
+        
+        func descriptionEnum() -> String
+        {
+            switch (self)
+            {
+            case .Tekst(let tekst):
+                return tekst
+            case .Obraz(let obraz):
+                return String(obraz)
+            case .Dźwięk(let dźwięk):
+                return String(dźwięk)
+            case .Współrzędne(let x, let y):
+                return "\(x), \(y)"
+            }
         }
         
-    
+    }
     
     init(awers:String,rewers:String,listaZPodpowiedziami:[typPodpowiedzi]){
         self.tekstAwers = awers
@@ -21,27 +38,32 @@ class Fiszka {
         self.listaPodpowiedzi = listaZPodpowiedziami
     }
     
-    
     func description()-> String {
         
         var podpowiedziString:String=""
+        let count = listaPodpowiedzi.count
         
-        for podpowiedz in listaPodpowiedzi {
-            podpowiedziString += "\(podpowiedz), "
+        for (index,podpowiedz) in listaPodpowiedzi.enumerate() {
+
+            if (index == count-1) {
+                podpowiedziString += "\(podpowiedz.descriptionEnum())"
+            }
+            else {
+                podpowiedziString += "\(podpowiedz.descriptionEnum()), "
+            }
+            
         }
-        
-        return "\(tekstAwers) -> \(tekstRewers) [ \(podpowiedziString) ]"
+    return "\(tekstAwers) -> \(tekstRewers) [ \(podpowiedziString) ]"
     }
-    
-    
-    
-    
 }
+
+let testowyObrazek:UIImage = UIImage(named: "Flag_Canada")!
 
 var podpowiedz1Test1:Fiszka.typPodpowiedzi = .Tekst(tekst: "Blisko USA")
 var podpowiedz2Test1:Fiszka.typPodpowiedzi = .Współrzędne(x: 62.746867, y: -120.773575)
+var podpowiedz3Test1:Fiszka.typPodpowiedzi = .Obraz(obraz: testowyObrazek)
 
-var test1 = Fiszka(awers: "Kanada", rewers: "Ameryka Północna", listaZPodpowiedziami: [podpowiedz1Test1,podpowiedz2Test1])
+var test1 = Fiszka(awers: "Kanada", rewers: "Ameryka Północna", listaZPodpowiedziami: [podpowiedz1Test1,podpowiedz2Test1,podpowiedz3Test1])
 
 
 test1.description()
--- Zadanie3.playground/Contents.swift
+++ Zadanie3.playground/Contents.swift
@@ -15,7 +15,22 @@ class Fiszka:DoNauczenia {
         case Obraz(obraz:UIImage)
         case Dźwięk(dźwięk:NSURL) //URL do dźwięku
         case Współrzędne(x:Float,y:Float) //przy pracy mapami (geografia, nawigacja morska)
+        
+        func descriptionEnum() -> String
+        {
+            switch (self)
+            {
+            case .Tekst(let tekst):
+                return tekst
+            case .Obraz(let obraz):
+                return String(obraz)
+            case .Dźwięk(let dźwięk):
+                return String(dźwięk)
+            case .Współrzędne(let x, let y):
+                return "\(x), \(y)"
+            }
         }
+    }
     
     init(awers:String,rewers:String,listaZPodpowiedziami:[typPodpowiedzi],czyNauczony:Bool){
         self.tekstAwers = awers
@@ -27,11 +42,18 @@ class Fiszka:DoNauczenia {
     func description()-> String {
         
         var podpowiedziString:String=""
+        let count = listaPodpowiedzi.count
         
-        for podpowiedz in listaPodpowiedzi {
-            podpowiedziString += "\(podpowiedz), "
+        for (index,podpowiedz) in listaPodpowiedzi.enumerate() {
+            
+            if (index == count-1) {
+                podpowiedziString += "\(podpowiedz.descriptionEnum())"
+            }
+            else {
+                podpowiedziString += "\(podpowiedz.descriptionEnum()), "
+            }
+            
         }
-        
         return "\(tekstAwers) -> \(tekstRewers) [ \(podpowiedziString) ]"
     }
     
@@ -42,10 +64,10 @@ var podpowiedz1Test1:Fiszka.typPodpowiedzi = .Tekst(tekst: "Blisko USA")
 var podpowiedz2Test1:Fiszka.typPodpowiedzi = .Współrzędne(x: 62.746867, y: -120.773575)
 
 //dla uproszczenia lista z podpowiedziami jest wszędzie taka sama
-var test1 = Fiszka(awers: "Kanada", rewers: "Ameryka Północna", listaZPodpowiedziami: [podpowiedz1Test1,podpowiedz2Test1],czyNauczony: false)
-var test2 = Fiszka(awers: "Nigeria", rewers: "Afryka", listaZPodpowiedziami: [podpowiedz1Test1,podpowiedz2Test1],czyNauczony: false)
-var test3 = Fiszka(awers: "Brazylia", rewers: "Ameryka Południowa", listaZPodpowiedziami: [podpowiedz1Test1,podpowiedz2Test1],czyNauczony: true)
-var test4 = Fiszka(awers: "Polska", rewers: "Europa", listaZPodpowiedziami: [podpowiedz1Test1,podpowiedz2Test1],czyNauczony: true)
+var test1 = Fiszka(awers: "Kanada", rewers: "Ameryka Płn",     listaZPodpowiedziami: [podpowiedz1Test1,podpowiedz2Test1],czyNauczony: false)
+var test2 = Fiszka(awers: "Nigeria", rewers: "Afryka",         listaZPodpowiedziami: [podpowiedz1Test1,podpowiedz2Test1],czyNauczony: false)
+var test3 = Fiszka(awers: "Brazylia", rewers: "Ameryka Płd",   listaZPodpowiedziami: [podpowiedz1Test1,podpowiedz2Test1],czyNauczony: true)
+var test4 = Fiszka(awers: "Polska", rewers: "Europa",          listaZPodpowiedziami: [podpowiedz1Test1,podpowiedz2Test1],czyNauczony: true)
 
 func wypiszNauczoneFiszki(tablicaInput:[Fiszka]) -> ([Fiszka]) {
     
